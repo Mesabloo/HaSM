fcf6af30aa4757eb5fac39ff1854b02fc101d8f5
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-11-30 11:02:30 +0100

    :white_check_mark: Updated tests

--- test/Register.hs
+++ test/Register.hs
@@ -1,18 +1,23 @@
 {-# LANGUAGE QuasiQuotes #-}
 
-import Language.HaSM (hasm)
+import Language.HaSM (hasm_x86)
 
 main :: IO ()
 main = do
     let y = 0x10
-    let x = [hasm| mov $0, %eax ; ret |]
-    let z = [hasm|
-            mov $0, %eax ;
+    let x = [hasm_x86| mov $0, %eax ; ret |]
+    let z = [hasm_x86|
+            mov $0x0, -0x0 ;
             ret ;;;;; ret
             label: nop |]
-    let a = [hasm| 
+    let a = [hasm_x86|
             label: # test comment
                    /* test multiline
                    comment */
                 ret |]
+
+    print x
+    print z
+    print a
+
     pure ()
