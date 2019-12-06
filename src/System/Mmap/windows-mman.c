b0707cc5bf188bc9ce326a1301458c4efc3206bd
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-12-06 15:59:14 +0100

    :checkered_flag: Windows compatibility working

--- src/System/Mmap/windows-mman.c
+++ src/System/Mmap/windows-mman.c
@@ -1,97 +1,179 @@
-/*
- *  IMS Open Corpus Workbench (CWB)
- *  Copyright (C) 1993-2006 by IMS, University of Stuttgart
- *  Copyright (C) 2007-     by the respective contributers (see file AUTHORS)
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by the
- *  Free Software Foundation; either version 2, or (at your option) any later
- *  version.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
- *  Public License for more details (in the file "COPYING", or available via
- *  WWW at http://www.gnu.org/copyleft/gpl.html).
- */
-/*
- * Windows/Unicode-compatibility extensions to CWB in this file
- *  Copyright (C) 2010      by ANR TextomÃ©trie, ENS de Lyon
- */
-
-/**
- * @file
- *
- * This file provides an equivalent of the POSIX mmap() function for use in
- * Windows; the Windows API lacks this function.
- *
- * The code was adapted from the Git project (note that Git is GPL-licensed).
- *
- * Very important note: this file should only be compiled and linked when
- * targeting a windows environment.
- *
- */
-
+#include <windows.h>
+#include <errno.h>
+#include <io.h>
 
 #include "windows-mman.h"
-#include <sys/stat.h>
 
-/**
- * Substitute POSIX mmap() for use in Windows.
- *
- * It wraps the Windows API functions CreateFileMapping() and
- * MapViewOfFileEx().
- *
- * The arguments, return, etc. are the same as POSIX mmap(), which is
- * documented much more thoroughly than we could hope to do here.
- */
-void* mmap(void* start, size_t length, int prot, int flags, int fd, off_t offset)
+#ifndef FILE_MAP_EXECUTE
+#define FILE_MAP_EXECUTE    0x0020
+#endif /* FILE_MAP_EXECUTE */
+
+static int __map_mman_error(const DWORD err, const int deferr)
 {
-    HANDLE hmap;
-    void* temp;
-    size_t len;
-    struct _stat st;
-    uint64_t o = offset;
-    uint32_t l = o & 0xFFFFFFFF;
-    uint32_t h = (o >> 32) & 0xFFFFFFFF;
-
-    if (!_fstat(fd, &st))
-        len = (size_t) st.st_size;
-    else {
-        fprintf(stderr,"mmap: could not determine filesize");
-        exit(1);
-    }
-
-    if ((length + offset) > len)
-        length = len - offset;
-
-    if (!(flags & MAP_PRIVATE)) {
-        fprintf(stderr,"Invalid usage of mmap when built with USE_WIN32_MMAP");
-        exit(1);
-    }
-
-    hmap = CreateFileMapping((HANDLE)_get_osfhandle(fd), 0, PAGE_WRITECOPY, 0, 0, 0);
-
-    if (!hmap)
-        return MAP_FAILED;
-
-    temp = MapViewOfFileEx(hmap, FILE_MAP_COPY, h, l, length, start);
-
-    if (!CloseHandle(hmap))
-        fprintf(stderr,"unable to close file mapping handle\n");
-    return temp ? temp : MAP_FAILED;
+    if (err == 0)
+        return 0;
+    //TODO: implement
+    return err;
 }
 
-
-
-/**
- * Substitute POSIX munmap() for environments that lack it.
- *
- * It wraps the Windows API function UnmapViewOfFile().
- *
- * @see mmap
- */
-int munmap(void* start, size_t length)
+static DWORD __map_mmap_prot_page(const int prot)
 {
-    return !UnmapViewOfFile(start);
+    DWORD protect = 0;
+    
+    if (prot == PROT_NONE)
+        return protect;
+        
+    if ((prot & PROT_EXEC) != 0)
+    {
+        protect = ((prot & PROT_WRITE) != 0) ? 
+                    PAGE_EXECUTE_READWRITE : PAGE_EXECUTE_READ;
+    }
+    else
+    {
+        protect = ((prot & PROT_WRITE) != 0) ?
+                    PAGE_READWRITE : PAGE_READONLY;
+    }
+    
+    return protect;
+}
+
+static DWORD __map_mmap_prot_file(const int prot)
+{
+    DWORD desiredAccess = 0;
+    
+    if (prot == PROT_NONE)
+        return desiredAccess;
+        
+    if ((prot & PROT_READ) != 0)
+        desiredAccess |= FILE_MAP_READ;
+    if ((prot & PROT_WRITE) != 0)
+        desiredAccess |= FILE_MAP_WRITE;
+    if ((prot & PROT_EXEC) != 0)
+        desiredAccess |= FILE_MAP_EXECUTE;
+    
+    return desiredAccess;
+}
+
+void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
+{
+    HANDLE fm, h;
+    
+    void * map = MAP_FAILED;
+    
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4293)
+#endif
+
+    const DWORD dwFileOffsetLow = (sizeof(off_t) <= sizeof(DWORD)) ? 
+                    (DWORD)off : (DWORD)(off & 0xFFFFFFFFL);
+    const DWORD dwFileOffsetHigh = (sizeof(off_t) <= sizeof(DWORD)) ?
+                    (DWORD)0 : (DWORD)((off >> 32) & 0xFFFFFFFFL);
+    const DWORD protect = __map_mmap_prot_page(prot);
+    const DWORD desiredAccess = __map_mmap_prot_file(prot);
+
+    const off_t maxSize = off + (off_t)len;
+
+    const DWORD dwMaxSizeLow = (sizeof(off_t) <= sizeof(DWORD)) ? 
+                    (DWORD)maxSize : (DWORD)(maxSize & 0xFFFFFFFFL);
+    const DWORD dwMaxSizeHigh = (sizeof(off_t) <= sizeof(DWORD)) ?
+                    (DWORD)0 : (DWORD)((maxSize >> 32) & 0xFFFFFFFFL);
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
+    errno = 0;
+    
+    if (len == 0 
+        /* Unsupported flag combinations */
+        || (flags & MAP_FIXED) != 0
+        /* Usupported protection combinations */
+        || prot == PROT_EXEC)
+    {
+        errno = EINVAL;
+        return MAP_FAILED;
+    }
+    
+    h = ((flags & MAP_ANONYMOUS) == 0) ? 
+                    (HANDLE)_get_osfhandle(fildes) : INVALID_HANDLE_VALUE;
+
+    if ((flags & MAP_ANONYMOUS) == 0 && h == INVALID_HANDLE_VALUE)
+    {
+        errno = EBADF;
+        return MAP_FAILED;
+    }
+
+    fm = CreateFileMapping(h, NULL, protect, dwMaxSizeHigh, dwMaxSizeLow, NULL);
+
+    if (fm == NULL)
+    {
+        errno = __map_mman_error(GetLastError(), EPERM);
+        return MAP_FAILED;
+    }
+  
+    map = MapViewOfFile(fm, desiredAccess, dwFileOffsetHigh, dwFileOffsetLow, len);
+
+    CloseHandle(fm);
+  
+    if (map == NULL)
+    {
+        errno = __map_mman_error(GetLastError(), EPERM);
+        return MAP_FAILED;
+    }
+
+    return map;
+}
+
+int munmap(void *addr, size_t len)
+{
+    if (UnmapViewOfFile(addr))
+        return 0;
+        
+    errno =  __map_mman_error(GetLastError(), EPERM);
+    
+    return -1;
+}
+
+int mprotect(void *addr, size_t len, int prot)
+{
+    DWORD newProtect = __map_mmap_prot_page(prot);
+    DWORD oldProtect = 0;
+    
+    if (VirtualProtect(addr, len, newProtect, &oldProtect))
+        return 0;
+    
+    errno =  __map_mman_error(GetLastError(), EPERM);
+    
+    return -1;
+}
+
+int msync(void *addr, size_t len, int flags)
+{
+    if (FlushViewOfFile(addr, len))
+        return 0;
+    
+    errno =  __map_mman_error(GetLastError(), EPERM);
+    
+    return -1;
+}
+
+int mlock(const void *addr, size_t len)
+{
+    if (VirtualLock((LPVOID)addr, len))
+        return 0;
+        
+    errno =  __map_mman_error(GetLastError(), EPERM);
+    
+    return -1;
+}
+
+int munlock(const void *addr, size_t len)
+{
+    if (VirtualUnlock((LPVOID)addr, len))
+        return 0;
+        
+    errno =  __map_mman_error(GetLastError(), EPERM);
+    
+    return -1;
 }
--- src/System/Mmap/windows-mman.h
+++ src/System/Mmap/windows-mman.h
@@ -1,40 +1,55 @@
-#ifndef WINDOWS_MMAN_H
-#define WINDOWS_MMAN_H
+/*
+ * sys/mman.h
+ * mman-win32
+ */
 
-#if !defined(_WIN32)
-#   error "The targetted OS is not Windows (either 32- or 64-bits)."
+#ifndef _SYS_MMAN_H_
+#define _SYS_MMAN_H_
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#endif						
+
+/* All the headers include this file. */
+#ifndef _MSC_VER
+#include <_mingw.h>
 #endif
 
-#include <windows.h>
-#include <stdint.h>
-#include <stdio.h>
 #include <sys/types.h>
 
-#define PROT_READ           0x1
-#define PROT_WRITE          0x2
-/* This flag is only available in WinXP+ */
-#ifdef FILE_MAP_EXECUTE
-#   define PROT_EXEC        0x4
-#else
-#   define PROT_EXEC        0x0
-#   define FILE_MAP_EXECUTE 0
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-#define MAP_SHARED          0x01
-#define MAP_PRIVATE         0x02
-#define MAP_ANONYMOUS       0x20
-#define MAP_ANON            MAP_ANONYMOUS
-#define MAP_FAILED          ((void *) -1)
+#define PROT_NONE       0
+#define PROT_READ       1
+#define PROT_WRITE      2
+#define PROT_EXEC       4
 
-#ifdef __USE_FILE_OFFSET64
-#   define DWORD_HI(x)      (x >> 32)
-#   define DWORD_LO(x)      ((x) & 0xffffffff)
-#else
-#   define DWORD_HI(x)      (0)
-#   define DWORD_LO(x)      (x)
+#define MAP_FILE        0
+#define MAP_SHARED      1
+#define MAP_PRIVATE     2
+#define MAP_TYPE        0xf
+#define MAP_FIXED       0x10
+#define MAP_ANONYMOUS   0x20
+#define MAP_ANON        MAP_ANONYMOUS
+
+#define MAP_FAILED      ((void *)-1)
+
+/* Flags for msync. */
+#define MS_ASYNC        1
+#define MS_SYNC         2
+#define MS_INVALIDATE   4
+
+void*   mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off);
+int     munmap(void *addr, size_t len);
+int     mprotect(void *addr, size_t len, int prot);
+int     msync(void *addr, size_t len, int flags);
+int     mlock(const void *addr, size_t len);
+int     munlock(const void *addr, size_t len);
+
+#ifdef __cplusplus
+};
 #endif
 
-void* mmap(void* start, size_t length, int prot, int flags, int fd, off_t offset);
-int munmap(void* start, size_t length);
-
-#endif
+#endif /*  _SYS_MMAN_H_ */
