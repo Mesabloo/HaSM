48a8d430a44b2a4aea720fffeb747a5bc2882288
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-12-14 18:25:02 +0100

    :white_check_mark: Updated tests

--- test/Register.hs
+++ test/Register.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE QuasiQuotes #-}
 
-import Language.HaSM (hasm_x86, run)
+import Language.HaSM
 import Data.Word (Word8)
 import Numeric (showHex)
 
 main :: IO ()
 main = do
-    let x = [hasm_x86| |]
-    let y = [hasm_x86|
+    let x = [hasm| |]
+    let y = [hasm|
 
 mov $5, %ebx
 add $5, %ebx
@@ -15,28 +15,35 @@ mov %ebx, %eax
 ret
 
         |]
-    let z = [hasm_x86|
+    let z = [hasm|
 
 mov $0, %ebx
 loop:
     add $1, %ebx
     jmp loop
+mov %ebx, %eax
+ret
+
+        |]
+    let a = [hasm|
+
+loop: jmp loop
 
         |]
 
-    dump x
-    dump y
-    dump z
+    print x
+    print y
+    print z
+    print a
 
-    res  <- run x
-    res2 <- run y
-    res3 <- run z -- < infinite loop
+    res  <- run X86 x
+    res2 <- run X86 y
+    res3 <- run X86 z -- < infinite loop
+    res4 <- run X86 a -- < infinite loop
 
-    print res
-    print res2
+    print res  -- < never happening
+    print res2 -- < never happening
     print res3 -- < never happening
+    print res4 -- < never happening
 
     pure ()
-
-dump :: [Word8] -> IO ()
-dump = print . fmap (("0x" <>) . (`showHex` ""))
